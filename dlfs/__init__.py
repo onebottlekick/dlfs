ede29b1500784b0bc5ef81833e8213de6ec4523d
onebottlekick <gksqudcks97@gmail.com>
2022-01-02 13:53:08 +0900

    added Function class

--- dlfs/__init__.py
+++ dlfs/__init__.py
@@ -1,1 +1,2 @@
 from dlfs.core import Variable
+from dlfs.core import Function, Square, Exp
---
+++ tests/test_functions.py
@@ -0,0 +1,26 @@
+import unittest
+
+import numpy as np
+
+from dlfs import Variable, Square, Exp
+
+
+x = Variable(np.array([10]))
+
+class FunctionTest(unittest.TestCase):
+    def test_square(self):
+        y = Square()(x)
+        self.assertTrue(x.data**2, y.data)        
+        
+    def test_exp(self):
+        y = Exp()(x)
+        self.assertTrue(np.exp(x.data), y.data)
+        
+    def test_composite_function(self):
+        A = Square()
+        B = Exp()
+        C = Square()
+        a = A(x)
+        b = B(a)
+        y = C(b)
+        self.assertTrue(np.exp(x.data**2)**2, y.data)
