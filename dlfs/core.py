87687c3d286e158a31d7959a3b328f780ad6fccf
onebottlekick <gksqudcks97@gmail.com>
2022-01-06 17:47:23 +0900

    added gradient tracking function
    - added grad variable in Variable class
    - added backward method in Function class
    - added test module

--- dlfs/core.py
+++ dlfs/core.py
@@ -4,6 +4,7 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
         
         
 class Function:
@@ -11,9 +12,13 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
     
     def forward(self, x):
+        raise NotImplementedError()
+    
+    def backward():
         raise NotImplementedError()
     
 
@@ -21,7 +26,17 @@ class Square(Function):
     def forward(self, x):
         return x ** 2
     
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2*x*gy
+        return gx
+    
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+    
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x)*gy
+        return gx
---
+++ tests/test_gradients.py
@@ -0,0 +1,34 @@
+import unittest
+
+import numpy as np
+
+from dlfs import Variable, Square, Exp
+
+
+x = Variable(np.array(0.5))
+
+
+def numerical_gradient(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
+
+class GradientTest(unittest.TestCase):
+    
+    def test_gradient(self):
+        A = Square()
+        B = Exp()
+        C = Square()
+        a = A(x)
+        b = B(a)
+        y = C(b)
+        
+        y.grad = 1.0
+        b.grad = C.backward(y.grad)
+        a.grad = B.backward(b.grad)
+        x.grad = A.backward(a.grad)
+
+        self.assertAlmostEqual(x.grad, numerical_gradient(lambda x: C(B(A(x))), x), places=6)
