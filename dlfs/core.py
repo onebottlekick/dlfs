17d072b08ed0f46c2bd1218760b4d2f3f48491b5
onebottlekick <gksqudcks97@gmail.com>
2022-01-23 21:42:19 +0900

    Functions class
    - single value -> vector

--- dlfs/core.py
+++ dlfs/core.py
@@ -31,14 +31,16 @@ class Variable:
         
         
 class Function:
-    def __call__(self, input):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self)
-        self.input = input
-        self.output = output
-        return output
+    def __call__(self, inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+        
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs
     
     def forward(self, x):
         raise NotImplementedError()
