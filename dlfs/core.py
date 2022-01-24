374813ecb983de4886ed04ebf6bd0faa449f6ac2
OneBottleKick <gksqudcks97@gmail.com>
2022-01-24 16:43:20 +0900

    Variable class
    - added cleargrad method

--- dlfs/core.py
+++ dlfs/core.py
@@ -32,10 +32,16 @@ class Variable:
                 gxs = (gxs,)
             
             for x, gx in zip(f.inputs, gxs):
-                x.grad = gx
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
             
                 if x.creator is not None:
                     funcs.append(x.creator)
+                    
+    def cleargrad(self):
+        self.grad = None
         
         
 class Function:
