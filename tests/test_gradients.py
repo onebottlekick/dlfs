2f0834dbcef055d4e92babd6a61ba1fce45fbc15
onebottlekick <gksqudcks97@gmail.com>
2022-02-06 13:25:26 +0900

    test function
    - sphere function

--- tests/test_gradients.py
+++ tests/test_gradients.py
@@ -53,3 +53,15 @@ class GradientTest(unittest.TestCase):
         
         self.assertTrue(y.data, 0.125)
         self.assertTrue(x.grad, 1.0)
+        
+        
+class ComplexFunctionTest(unittest.TestCase):
+    def test_sphere(self):
+        def sphere(x, y):
+            z = x**2 + y**2
+            return z
+        x = Variable(np.array(1.0))
+        y = Variable(np.array(1.0))
+        z = sphere(x, y)
+        z.backward()
+        self.assertTrue(x.grad, 2.0)
