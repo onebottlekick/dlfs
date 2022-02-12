9c0e5949ac9afad071fa32a398f746526be7b59e
onebottlekick <gksqudcks97@gmail.com>
2022-02-12 13:12:38 +0900

    functions
    - Reshape
    - Transpose

--- dlfs/functions.py
+++ dlfs/functions.py
@@ -1,6 +1,7 @@
+from tkinter import Y
 import numpy as np
 
-from core import Function
+from core import Function, as_variable
 
 
 class Tanh(Function):
@@ -12,7 +13,39 @@ class Tanh(Function):
         y = self.outputs[0]()
         gx = gy*(1 - y*y)
         return gx
+
+
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape
+        
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.reshape(self.shape)
+        return y
     
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+    
+    
+class Transpose(Function):
+    def forward(self, x):
+        y = np.transpose(x)
+        return Y
+    
+    def backward(self, gy):
+        gx = transpose(gy)
+        return gx
+
 
 def tanh(x):
     return Tanh()(x)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
+
+def transpose(x):
+    return Transpose()(x)
