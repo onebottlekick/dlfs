bdbf28fb2b3c87e0a43416d4bd542f18e6e7753a
onebottlekick <gksqudcks97@gmail.com>
2022-02-28 14:54:39 +0900

    updated Layer.params method

--- dlfs/layers.py
+++ dlfs/layers.py
@@ -29,7 +29,12 @@ class Layer:
     
     def params(self):
         for name in self._params:
-            yield  self.__dict__[name]
+            obj =  self.__dict__[name]
+
+            if isinstance(obj, Layer):
+                yield from obj.params()
+            else:
+                yield obj
 
     def cleargrads(self):
         for param in self.params():
