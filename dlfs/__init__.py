0920f83817364f0b67e97b5eca5c20480c4b1fc1
onebottlekick <gksqudcks97@gmail.com>
2022-02-28 15:00:56 +0900

    implemented Model class

--- dlfs/__init__.py
+++ dlfs/__init__.py
@@ -1,3 +1,5 @@
 from dlfs.core import *
 from dlfs.utils import *
 from dlfs.functions import *
+from dlfs.layers import *
+from dlfs.models import *
---
+++ dlfs/models.py
@@ -0,0 +1,8 @@
+from dlfs import Layer
+from dlfs import utils
+
+
+class Model(Layer):
+    def plot(self, *inputs, to_file='model.png'):
+        y = self.forward(*inputs)
+        return utils.plot_dot_graph(y, verbose=True, to_file=to_file)
