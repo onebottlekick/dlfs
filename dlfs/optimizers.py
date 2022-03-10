13c7bc613bfddfc2366128eb5685036773cb4417
onebottlekick <gksqudcks97@gmail.com>
2022-03-10 21:12:33 +0900

    added optimizer
    - Adam

--- dlfs/optimizers.py
+++ dlfs/optimizers.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -51,3 +53,39 @@ class MomentumSGD(Optimizer):
         v *= self.momentum
         v -= self.lr*param.grad.data
         param.data += v
+        
+        
+class Adam(Optimizer):
+    def __init__(self, alpha=0.01, beta1=0.9, beta2=0.999, eps=1e-8):
+        super(Adam, self).__init__()
+        self.t = 0
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.eps = eps
+        self.ms = {}
+        self.vs = {}
+        
+    def update(self, *args, **kwargs):
+        self.t += 1
+        super().update(*args, **kwargs)
+        
+    @property
+    def lr(self):
+        fix1 = 1. - math.pow(self.beta1, self.t)
+        fix2 = 1. - math.pow(self.beta2, self.t)
+        return self.alpha*math.sqrt(fix2)/fix1
+    
+    def update_one(self, param):
+        key = id(param)
+        if key not in self.ms:
+            self.ms[key] = np.zeros_like(param.data)
+            self.vs[key] = np.zeros_like(param.data)
+            
+        m, v = self.ms[key], self.vs[key]
+        beta1, beta2, eps = self.beta1, self.beta2, self.eps
+        grad = param.grad.data
+        
+        m += (1 - beta1)*(grad - m)
+        v += (1 - beta2)*(grad*grad - v)
+        param.data -= self.lr*m/(np.sqrt(v) + eps)
