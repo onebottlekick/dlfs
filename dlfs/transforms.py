950d91350f798401ad2c97fe74cf4f8f0956d95b
onebottlekick <gksqudcks97@gmail.com>
2022-03-13 12:46:43 +0900

    transforms
    - ToArray

--- dlfs/transforms.py
+++ dlfs/transforms.py
@@ -13,3 +13,19 @@ class Compose:
         for t in self.transforms:
             img = t(img)
         return img
+    
+    
+class ToArray:
+    def __init__(self, dtype=np.float32):
+        self.dtype = dtype
+        
+    def __call__(self, img):
+        if isinstance(img, np.ndarray):
+            return img
+        if isinstance(img, Image.Image):
+            img = np.asarray(img)
+            img = img.transpose(2, 0, 1)
+            img = img.astype(self.dtype)
+            return img
+        else:
+            raise TypeError
