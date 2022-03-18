47aa4e709c5f4e26c5134b7bac21d6c494bfc07c
onebottlekick <gksqudcks97@gmail.com>
2022-03-18 16:03:12 +0900

    transforms
    - Normalize

--- dlfs/transforms.py
+++ dlfs/transforms.py
@@ -87,3 +87,23 @@ class AsType:
     
     def __call__(self, array):
         return array.astype(self.dtype)
+    
+    
+class Normalize:
+    def __init__(self, mean=0, std=1):
+        self.mean = mean
+        self.std = std
+        
+    def __call__(self, array):
+        mean, std = self.mean, self.std
+        
+        if not np.isscalar(mean):
+            mshape = [1]*array.ndim
+            mshape[0] = len(array) if len(self.mean) == 1 else len(self.mean)
+            mean = np.array(self.mean, dtype=array.dtype).reshape(*mshape)
+            
+        if not np.isscalar(std):
+            rshape = [1]*array.ndim
+            rshape[0] = len(array) if len(self.std) == 1 else len(self.std)
+            std = np.array(self.std, dtype=array.dtype).reshape(*rshape)
+        return (array - mean)/std
