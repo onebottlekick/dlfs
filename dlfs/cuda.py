b98b8d0b70fbb03510841409fd1038ba89ec9161
onebottlekick <gksqudcks97@gmail.com>
2022-03-31 23:40:41 +0900

    cuda.py
    - get_array_module()
    - as_numpy()

--- dlfs/cuda.py
+++ dlfs/cuda.py
@@ -10,3 +10,21 @@ except ImportError:
     gpu_enable = False
     
 from dlfs import Variable
+
+
+def get_array_module(x):
+    if isinstance(x, Variable):
+        x = x.data
+        
+    if not gpu_enable:
+        return np
+    xp = cp.get_array_module(x)
+    return xp
+
+
+def as_numpy(x):
+    if isinstance(x, Variable):
+        x = x.data
+        
+    if np.isscalar(x):
+        return np.array(x)
